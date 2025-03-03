c7b2e8d9b9f584e33ef870f86d2c7186f924df29
Tooru Wu <tooruwu@ToorudeMacBook-Air.local>
2025-03-03 10:42:18 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html","./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily:{
+        pingfang: ['PingFang TC', 'Noto Sans TC', 'Microsoft JhengHei', 'sans-serif'],
+      }
+    },
+  },
+  plugins: [],
+}
+
